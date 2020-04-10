6fe20e6c6bcce1ed28fead12f21fec28cd4fc565
hyochan-Kim <kom1230@hanyang.ac.kr>
2020-04-10 17:36:41 +0900

    [feat] 전체 진행하는 로직 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,83 @@
-import domain.Menu;
-import domain.MenuRepository;
-import domain.Table;
-import domain.TableRepository;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
-    // TODO 구현 진행
+
     public static void main(String[] args) {
-        final List<Table> tables = TableRepository.tables();
+        boolean isNotEnd = true;
+        while (isNotEnd) {
+            isNotEnd = run();
+        }
+    }
+
+    private static boolean run() {
+        OutputView.printCommand();
+        Command command = Command.of(InputView.inputCommand());
+        if (command == Command.ORDER) {
+            order();
+        }
+
+        if (command == Command.PAY) {
+            pay();
+        }
+
+        if (command == Command.END) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void order() {
+        Tables tables = new Tables(TableRepository.tables());
         OutputView.printTables(tables);
 
         final int tableNumber = InputView.inputTableNumber();
 
-        final List<Menu> menus = MenuRepository.menus();
+        Menus menus = new Menus(MenuRepository.menus());
         OutputView.printMenus(menus);
+
+        final int menuNumber = InputView.inputMenuNumber();
+        final int menuAmount = InputView.inputAmount();
+
+        tables.addOrderToTable(tableNumber, menus.of(menuNumber), menuAmount);
+    }
+
+    private static void pay() {
+        final Tables tables = new Tables(TableRepository.tables());
+        OutputView.printTables(tables);
+
+        final int tableNumber = InputView.inputTableNumber();
+
+        Map<Menu, Integer> orderHistory = tables.of(tableNumber).getOrderHistory();
+        OutputView.printOrderistory(makeOrderHistory(orderHistory));
+
+        Price totalPrice = Payment.calculateTotalPrice(tables.of(tableNumber).getOrderHistory());
+        final PaymentMethod paymentMethod = PaymentMethod.of(InputView.inputPaymentMethod());
+        if (paymentMethod == PaymentMethod.CASH) {
+            totalPrice = Payment.discountCash(totalPrice);
+        }
+
+        OutputView.printTotalPrice(totalPrice);
+        tables.pay(tableNumber);
+    }
+
+    private static List<OrderDto> makeOrderHistory(final Map<Menu, Integer> orderHistory) {
+        List<OrderDto> orderDtos = new ArrayList<>();
+        for (Map.Entry<Menu, Integer> entry : orderHistory.entrySet()) {
+            Menu menu = entry.getKey();
+            int count = entry.getValue();
+            OrderDto orderDto = new OrderDto();
+            orderDto.setMenuName(menu.getName());
+            orderDto.setCount(count);
+            orderDto.setTotalPrice(menu.getPrice().multiply(count));
+            orderDtos.add(orderDto);
+        }
+        return orderDtos;
     }
 }
